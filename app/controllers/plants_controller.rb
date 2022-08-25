a5776dfd354b3e672bf85949b5f260e4ce975de9
ajlic24@gmail.com <ajlic24@gmail.com>
2022-08-25 17:42:29 -0400

    commit tree 06ce17fdf5e9fe7b6d9b336081726a8913c6b42d

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -8,8 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    render json: find_plant
   end
 
   # POST /plants
@@ -18,7 +18,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: :ok
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: {error: 'Plant not found'}, status: :not_found
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
